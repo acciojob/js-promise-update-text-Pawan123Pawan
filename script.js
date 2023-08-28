15987398c17a8ebf548e52ff267a950118dddbd1
Pawan123Pawan <105289150+Pawan123Pawan@users.noreply.github.com>
2023-08-29 02:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ function delayedHello() {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve('Hello, world!');
+                }, 1000); // 1000 milliseconds (1 second)
+            });
+        }
+
+// Call the delayedHello function and update the HTML element when the Promise resolves
+delayedHello()
+.then((message) => {
+document.getElementById('output').textContent = message;
+});
